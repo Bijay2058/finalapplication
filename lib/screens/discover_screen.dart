87781d1283cbafb4964d572c359a77f4e9d56f84
unknown <bijaykhadka722@gmail.com>
2023-05-13 08:35:39 +0545

    tabchanges to 2 in home

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
@@ -13,46 +14,11 @@ import 'package:untitled2/widgets/bottom_navigation_bar.dart';
 
 import '../model/model.dart';
 
-User? user = FirebaseAuth.instance.currentUser;
-UserModel currentuser = UserModel();
-
-Widget drawer(BuildContext context) {
-  {
-    Future<void> signOut() async {
-      await FirebaseAuth.instance.signOut();
-      GoRouter.of(context).replace('/a');
-    }
-
-
-    return Drawer(
-
-        child:Container(
-            color:Color.fromRGBO(17, 26, 41, 1),
-            padding: EdgeInsets.fromLTRB(10, 60, 0, 0),
-
-            child: Column(
-
-              children: <Widget>[ListTile(
-                  leading: CircleAvatar(
-                      child: Icon(CupertinoIcons.person, color: Colors.white)),
-                  title: Text('${currentuser.username}',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)
-
-              ),
-                SizedBox(height: 40),
-
-
-                ListTile(leading:Icon(Icons.recommend,color: Colors.white,),title:Text("Recommendation",style:TextStyle(color:Colors.white,)),onTap: (){},),
-                ListTile(leading:Icon(Icons.favorite,color: Colors.white,),title:Text("Favourites",style:TextStyle(color:Colors.white)),onTap: (){},),
-                ListTile(leading:Icon(Icons.logout,color: Colors.white,),title:Text("Logout",style: TextStyle(color: Colors.white,)),onTap: (){signOut();},),
-
-              ],)
-        )
+var userid = "Demo";
 
 
 
-    );
-  }
-}
+
 
 class discoverscreen extends StatefulWidget {
 
@@ -64,11 +30,70 @@ class discoverscreen extends StatefulWidget {
 }
 
 class _discoverscreenState extends State<discoverscreen> {
+
   final GlobalKey<NavigatorState> _newsNavigatorKey = GlobalKey<NavigatorState>();
+
+  Future<void> activeuser() async {
+    await FirebaseFirestore.instance.collection("users").doc(user?.uid)
+        .get()
+        .then((value) {
+      currentuser = UserModel.fromMap(value.data());
+    });
+    setState(() {
+
+
+    });
+  }
+
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel currentuser = UserModel();
+
+
+
+  Widget drawer(BuildContext context) {
+    {
+      Future<void> signOut() async {
+        await FirebaseAuth.instance.signOut();
+        GoRouter.of(context).replace('/l');
+      }
+
+      activeuser();
+
+
+      return Drawer(
+
+          child:Container(
+              color:Color.fromRGBO(17, 26, 41, 1),
+              padding: EdgeInsets.fromLTRB(10, 60, 0, 0),
+
+              child: Column(
+
+                children: <Widget>[ListTile(
+                    leading: CircleAvatar(
+                        child: Icon(CupertinoIcons.person, color: Colors.white)),
+                    title: Text('${currentuser.username}',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)
+
+                ),
+                  SizedBox(height: 40),
+
+
+                  ListTile(leading:Icon(Icons.recommend,color: Colors.white,),title:Text("Recommendation",style:TextStyle(color:Colors.white,)),onTap: (){},),
+                  ListTile(leading:Icon(Icons.favorite,color: Colors.white,),title:Text("Favourites",style:TextStyle(color:Colors.white)),onTap: (){},),
+                  ListTile(leading:Icon(Icons.logout,color: Colors.white,),title:Text("Logout",style: TextStyle(color: Colors.white,)),onTap: (){signOut();},),
+
+                ],)
+          )
+
+
+
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 4,
+
+    return DefaultTabController(length: 2,
            child:Scaffold(
                drawer:drawer(context),
     appBar: AppBar(
@@ -81,12 +106,12 @@ class _discoverscreenState extends State<discoverscreen> {
     }));
     },
     icon: Icon(Icons.search))],
-    backgroundColor: Color.fromRGBO(17, 26, 41, 0.9),),
+    backgroundColor: Color.fromRGBO(17, 26, 41, 1),),
 
 
 
              body:Container(
-               color: Color.fromRGBO(17, 26, 41, 0.9),
+               color: Color.fromRGBO(17, 26, 41, 1),
                child: Column(
 
                  children: [TabBar(tabs: [
@@ -96,12 +121,7 @@ class _discoverscreenState extends State<discoverscreen> {
                    Tab(
                      child: Center(child: (Text('Genre',style: TextStyle(fontSize: 12),))),
                    ),
-                   Tab(
-                     child: Center(child: (Text('Categories',style: TextStyle(fontSize: 12),))),
-                   ),
-                   Tab(
-                     child: Center(child: (Text('Characters',style: TextStyle(fontSize: 12),))),
-                   ),
+
 
                  ]),
 
@@ -134,5 +154,7 @@ class _discoverscreenState extends State<discoverscreen> {
     );
   }
 }
+
+
 
 
